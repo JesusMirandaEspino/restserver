bc1123ef0f8be4b2705a5f07ecc4aa1ee6b5f0cc
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-22 21:15:10 -0600

    Conect to server put, get, post & delete

--- models/server.js
+++ models/server.js
@@ -1,22 +1,53 @@
 const express = require( 'express' );
 
-
+//TODO clase server 
 class Server {
 
 constructor(){
 
+    //variables para asignar express
     this.app = express();
     this.port = process.env.PORT;
+
+
+    //middlewares
+    this.middlewares();
 
     this.routes();
 }
 
 
+middlewares(){
+
+    //directorio publico
+    this.app.use( express.static( 'public' ) );
+}
+
 routes(){
 
-    this.app.get( '/', ( req, res ) =>  {
-    res.send('Hello world');
-} );
+    this.app.get( '/api', ( req, res ) =>  {
+    res.json( {
+        msg: 'Get API'
+        });
+    });
+
+    this.app.put( '/api', ( req, res ) =>  {
+    res.json( {
+        msg: 'Put API'
+        });
+    });
+
+    this.app.post( '/api', ( req, res ) =>  {
+    res.json( {
+        msg: 'Post API'
+        });
+    });
+
+    this.app.delete( '/api', ( req, res ) =>  {
+    res.json( {
+        msg: 'Delete API'
+        });
+    });
 
 
 }
@@ -24,7 +55,9 @@ routes(){
 
 
 listen(){
-    this.app.listen( port  );
+    this.app.listen( this.port, () => {
+        console.log( 'Servidor corriendo en el puerto', this.port );
+    }  );   
 }
 
 
